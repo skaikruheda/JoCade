a183d8c785b120391f9f4ee841f0d5ef9cefa920
skaikruheda <jonibekkar@gmail.com>
2023-03-08 00:25:33 -0500

    Implemented Shop Page for Flappy Jo Game
    Added the option to view shop for Flappy Jo. Also styled it so its reponsive on larger displays

--- JAVASCRIPT/flappyjo.js
+++ JAVASCRIPT/flappyjo.js
@@ -9,18 +9,15 @@ document.querySelector('#game-coin').textContent = 'JOCOINS: ' + currentUser.poi
 let flappyJoContainer = document.querySelector('.flappy-jo-container');
 let startButton;
 
-function setupGameMenu() {
+function gameMenu() {
+    flappyJoContainer.style.backgroundColor = 'rgb(241, 247, 181)';
     flappyJoContainer.innerHTML = `
         <h1>Flappy Jo</h1>
         <button id="start-btn">Start</button>
         <button id="marketplace-btn">Marketplace</button>
         <button id="setting-btn">Settings</button>
     `;
-    startButton = document.querySelector('#start-btn');
-    startButton.addEventListener('click', startGame);
 }
-
-setupGameMenu();
 
 function startGame() {
     flappyJoContainer.innerHTML = `
@@ -28,48 +25,34 @@ function startGame() {
         <div id="hole"></div>
         <div id="character"></div>
     `;
-    
-    let block = document.querySelector('#block');
-    let hole = document.querySelector('#hole');
-    let character = document.querySelector('#character');
-    let jumping = 0;
-    let counter = 0;
-
-    hole.addEventListener('animationiteration', () => {
-        const choices = [-40, -50, -60, -70, -80, -90];
-        let random = Math.floor(Math.random() * 6);
-        hole.style.top = choices[random] + '%';
-        counter++;
-    });
-
-    setInterval(() => {
-        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
-        if (jumping === 0) {
-            character.style.top = (characterTop + 3) + 'px';
-        }
-        if (characterTop > 633 || characterTop < 171) {
-            character.style.top = 50 + '%';
-            counter = 0;
-            setupGameMenu();
-        }
-    }, 10);
-
-    flappyJoContainer.addEventListener('click', jump);
-
-    function jump() {
-        jumping = 1;
-        let jumpCount = 0;
-        let jumpInterval = setInterval(() => {
-            let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
-            if (characterTop > 6 && jumpCount < 15) {
-                character.style.top = (characterTop - 5) + 'px';
-            }
-            if (jumpCount > 20) {
-                clearInterval(jumpInterval);
-                jumping = 0;
-                jumpCount = 0;
-            }
-            jumpCount++;
-        }, 10);
-    }
 }
+
+function shopeMenu() {
+    flappyJoContainer.style.backgroundColor = 'rgb(253, 138, 138)';
+    flappyJoContainer.innerHTML = `
+        <h1 id="shop-title">Shop</h1>
+        <h2 class="flappy-shop">Character</h2>
+        <div class="character-options">
+            <div class="character-btn">Red</div>
+            <div class="character-btn">Red</div>
+            <div class="character-btn">Red</div>
+            <div class="character-btn">Red</div>
+        </div>
+        <h2 class="flappy-shop">Obstacle</h2>
+        <div class="obstacle-options">
+            <div class="character-btn">Red</div>
+            <div class="character-btn">Red</div>
+            <div class="character-btn">Red</div>
+            <div class="character-btn">Red</div>
+        </div>
+        <h2 class="flappy-shop">Background</h2>
+        <div class="background-options">
+            <div class="character-btn">Red</div>
+            <div class="character-btn">Red</div>
+            <div class="character-btn">Red</div>
+            <div class="character-btn">Red</div>
+        </div>
+    `;
+}
+
+shopeMenu();
