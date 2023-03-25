936d2f19e98a0fe28162eba252da07cc7c98323f
skaikruheda <jonibekkar@gmail.com>
2023-03-24 23:07:53 -0400

    Added Player, Created Class for Ease of Growth, and Utilized Canvas

--- JAVASCRIPT/crazyassault.js
+++ JAVASCRIPT/crazyassault.js
@@ -8,6 +8,34 @@ if (currentUserFromLocalStorage) {
 document.querySelector('#game-coin').textContent = 'JOCOINS: ' + currentUser.points;
 
 const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth * 0.75;
+canvas.height = window.innerHeight * 0.75;
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth * 0.75;
+    canvas.height = window.innerHeight * 0.75;
+});
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, 'blue');
+
+player.draw();
