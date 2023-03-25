6535620ce066823d087a1dae11a7fb578f2f1bab
skaikruheda <jonibekkar@gmail.com>
2023-03-25 19:50:02 -0400

    Implemented Attack Beam Logic  for Crazy Assault

--- JAVASCRIPT/crazyassault.js
+++ JAVASCRIPT/crazyassault.js
@@ -10,12 +10,12 @@ document.querySelector('#game-coin').textContent = 'JOCOINS: ' + currentUser.poi
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-canvas.width = window.innerWidth * 0.75;
-canvas.height = window.innerHeight * 0.75;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth * 0.75;
-    canvas.height = window.innerHeight * 0.75;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 });
 
 class Player {
@@ -33,9 +33,41 @@ class Player {
     }
 }
 
+class Debree {
+    constructor(x, y, radius, color, speed) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+    }
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+    update() {
+        this.x = this.x + this.speed.x;
+        this.y = this.y + this.speed.y;
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
 
 player.draw();
+
+const debree = new Debree(canvas.width / 2, canvas.height / 2, 5, 'red', { x: 1, y: 1 });
+
+function animate() {
+    requestAnimationFrame(animate);
+    debree.draw();
+    debree.update();
+}
+
+canvas.addEventListener('click', function(event) {});
+
+animate();
